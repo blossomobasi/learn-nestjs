8b76e5215e8e121028b20d3cd035bbe2b6eeb9da
Obasi Chizitere <blossomobasi2@gmail.com>
2024-10-20 16:47:52 +0100

    Update: remove logger import

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
-import { MyLoggerModule } from './my-logger/my-logger.module';
 
 @Module({
   imports: [
@@ -25,7 +24,6 @@ import { MyLoggerModule } from './my-logger/my-logger.module';
         limit: 100,
       },
     ]),
-    MyLoggerModule,
   ],
   controllers: [AppController],
   providers: [
